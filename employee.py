fb1e4eeec60dd69e699178b5284443e96ef54ada
Areesha <areesha.durrani@hotmail.com>
2022-10-27 17:40:42 +0100

    Create files for new classes

--- employee.py
+++ employee.py
@@ -1,6 +1,16 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+class _Employee:
+    def __init__(self, name):
+        self.name = name
+
+    def get_pay(self):
+        pass
+
+    def __str__(self):
+        return self.name
+
 class Employee:
     def __init__(self, name, contract,pay, hasCommission):
       self.name = name
@@ -73,19 +83,23 @@ class Employee:
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
+billie = _Employee('Billie')
 billie = Employee('Billie', 'monthly',4000, False)
 
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
+charlie = _Employee('Charlie')
 charlie = Employee('Charlie', 'hourly',25, False)
 charlie.setHours(100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
+renee = _Employee('Renee')
 renee = Employee('Renee', 'monthly',3000, True)
 renee.setCommissionType('contract')
 renee.setCommissionContracts(4,200)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
+jan = _Employee('Jan')
 jan = Employee('Jan','hourly',25,True)
 jan.setHours(150)
 jan.setCommissionType('contract')
@@ -94,11 +108,13 @@ jan.setCommissionContracts(3,220)
 
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
+robbie = _Employee('Robbie')
 robbie = Employee('Robbie','monthly',2000,True)
 robbie.setCommissionType('bonus')
 robbie.setBonusPay(1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
+ariel = _Employee('Ariel')
 ariel = Employee('Ariel','hourly',30,True)
 ariel.setHours(120)
 ariel.setCommissionType('bonus')
